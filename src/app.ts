4c0197508341d35dbffbd542aa040c885a249fde
aphisit khamya <voraphol12345@gmail.com>
2025-06-12 15:44:00 +0700

    add landing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import path from "path";
 import userRoutes from "./routes/user";
 
 const app = express();
 const PORT = 3000;
 
-app.use("/", userRoutes);
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`[🎉API] Server is running on http://localhost:${PORT}`);
